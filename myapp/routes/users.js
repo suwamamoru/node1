7a305a9b9a6fe8462719a1be544b1be86ff0beb8
諏訪守 <suwamamoru@suwamamorunoMacBook-Air.local>
2022-02-08 10:32:11 +0900

    ERROR: connect ECONNREFUSED 127.0.0.1:3306

---
+++ myapp/routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+var userController = require('../controllers/user')
+
+/* GET users listing. */
+router.get('/', userController.showAllUsers);
+router.get('/:userId', userController.showUserById);
+router.get('/json', userController.sendJson);
+/*
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+*/
+module.exports = router;
